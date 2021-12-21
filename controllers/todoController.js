74be274b0ffb81d36df00e4a69e658a1c30c15b9
alexandra okoro <alex.nnenna.okoro@gmail.com>
2021-12-21 03:12:45 +0000

    api endpoints linked with mongodb

--- controllers/todoController.js
+++ controllers/todoController.js
@@ -1,8 +1,7 @@
-let data = [{ item: 'Cook' }, { item: 'Code' }, { item: 'Sleep' }, { item: 'Repeat' }]
 const mongoose = require('mongoose');
 
 //Connect to database
-mongoose.connect('mongodb+srv://test:test@todos.vxfqs.mongodb.net/myFirstDatabase?retryWrites=true&w=majority')
+mongoose.connect('ADD_DB_URL_WITH_USERNAME_AND_PASSWORD')
 
 //Create a schema
 const todoSchema = new mongoose.Schema({
@@ -11,23 +10,26 @@ const todoSchema = new mongoose.Schema({
 
 //Create model
 const Todo = mongoose.model('Todo', todoSchema);
-const itemOne = Todo({item: "Cook"}).save((err)=>{
-  if(err) throw err;
-  console.log('item saved');
-});
 
 module.exports = function (app) {
   app.get('/todo', function(req,res){
-    res.render('todo', {todos: data})
+    Todo.find({}, (err, data)=>{
+      if(err) throw err;
+      res.render('todo', {todos: data})
+    });
   })
 
   app.post('/todo', function (req, res) {
-    data.push(req.body);
-    res.json({ todos: data });
+    const newTodo = Todo(req.body).save((err, data)=> {
+      if(err) throw err;
+      res.json({ todos: data });
+    });
   })
 
   app.delete('/todo/:item', function (req, res) {
-    data = data.filter((todo) => todo.item.replace(/ /g, "-") !== req.params.item);
-    res.json({ todos: data });
-  })
+    Todo.find({item: req.params.item.replace(/\-/g, " ")}).remove((err, data)=>{
+      if(err) throw err;
+      res.json({ todos: data });
+    });
+  });
 };
