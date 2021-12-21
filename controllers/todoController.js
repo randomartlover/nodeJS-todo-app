3e974dd8149c2143d4f42944aa69345339d1698a
alexandra okoro <alex.nnenna.okoro@gmail.com>
2021-12-21 02:58:15 +0000

    mongoose dependency installed, mongoose setup with mongoDb done

--- controllers/todoController.js
+++ controllers/todoController.js
@@ -1,4 +1,20 @@
 let data = [{ item: 'Cook' }, { item: 'Code' }, { item: 'Sleep' }, { item: 'Repeat' }]
+const mongoose = require('mongoose');
+
+//Connect to database
+mongoose.connect('mongodb+srv://test:test@todos.vxfqs.mongodb.net/myFirstDatabase?retryWrites=true&w=majority')
+
+//Create a schema
+const todoSchema = new mongoose.Schema({
+  item: String
+});
+
+//Create model
+const Todo = mongoose.model('Todo', todoSchema);
+const itemOne = Todo({item: "Cook"}).save((err)=>{
+  if(err) throw err;
+  console.log('item saved');
+});
 
 module.exports = function (app) {
   app.get('/todo', function(req,res){
